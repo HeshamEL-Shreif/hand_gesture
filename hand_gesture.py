8bc561d4ba26ee564c0cf612f26f0accfca2ef02
Hesham EL_Sherif <107759753+HeshamEL-Shreif@users.noreply.github.com>
2025-03-25 02:50:31 +0200

    hand gesture

---
+++ hand_gesture.py
@@ -0,0 +1,50 @@
+
+import cv2 
+import mediapipe as mp
+import joblib
+from preprocess_landmark import preprocess_landmarks
+
+model_path = "rf_model.pkl"
+model = joblib.load(model_path)  
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+hands = mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+capture = cv2.VideoCapture(0, cv2.CAP_AVFOUNDATION)
+if not capture.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+while True:
+    ret, frame = capture.read()
+    if not ret:
+        break
+
+    frame = cv2.resize(frame, (640, 480))
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = hands.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+
+            landmark_vector = preprocess_landmarks(hand_landmarks.landmark)
+            if landmark_vector is None:
+                continue  
+
+            prediction = model.predict([landmark_vector])[0]
+
+            cv2.putText(image, f"Prediction: {prediction}", (10, 50), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    cv2.imshow("Hand Landmarks", image)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
---
+++ preprocess_landmark.py
@@ -0,0 +1,22 @@
+import numpy as np
+import joblib
+
+scaler_path = "scaler.pkl"
+scaler = joblib.load(scaler_path) 
+
+def preprocess_landmarks(hand_landmarks):
+    points = np.array([[lm.x, lm.y, lm.z] for lm in hand_landmarks])
+
+    if points.shape != (21, 3):  
+        print(f"Error: Expected 21 landmarks, but got shape {points.shape}. Skipping frame.")
+        return None  
+    wrist = points[0, :2]
+    mid_finger_tip = points[12, :2]
+    points[:, :2] -= wrist 
+    points[:, :2] /= mid_finger_tip  
+    
+    points = points.flatten().reshape(1, -1) 
+
+    points = scaler.transform(points)  
+
+    return points.flatten() 
